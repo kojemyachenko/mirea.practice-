2d2834205f94aff6f39fc3fa18b161d9e39f4e7c
kojemyachenko <kozemacenkonikola@gmail.com>
2024-09-19 14:32:29 +0300

    Add files via upload

---
+++ practice1_kojemyachenko/src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+    double height = -1;
+    double width = -1;
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    double getArea(){
+        return height * width;
+    }
+
+    double getPerimeter(){
+        return (height + width) * 2;
+    }
+
+}
---
+++ practice1_kojemyachenko/src/Stock.java
@@ -0,0 +1,17 @@
+public class Stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+    double getChangePercent(){
+        return (currentPrice-previousClosingPrice)/previousClosingPrice*100;
+    }
+
+}
