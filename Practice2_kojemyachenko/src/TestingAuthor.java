cb04ee338a2cd3a59f1b1fb43fb29031241363a0
kojemyachenko <kozemacenkonikola@gmail.com>
2024-09-19 14:49:52 +0300

    Add files via upload

---
+++ Practice2_kojemyachenko/src/TestingAuthor.java
@@ -0,0 +1,6 @@
+public class TestingAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Kirill", "Kirillkong@mail.ru", 'M');
+        System.out.println(a1);
+    }
+}
